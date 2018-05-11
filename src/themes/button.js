f19f5322e8ed94203c1b13d73443420b5f6d2895
Donald Silveira <donaldsilveira@gmail.com>
2018-05-11 11:32:55 -0300

    Modulating button styles

---
+++ src/themes/button.js
@@ -0,0 +1,36 @@
+import { INCLUDE } from '@shoutem/theme';
+import { defaultVariables as variables } from './variables';
+
+export default {
+  'lh.ui.Button': {
+    [INCLUDE]: ['commonVariants', 'guttersMargin'],
+
+    '.full-width': {
+      flex: 1,
+      alignSelf: 'stretch',
+      height: 52,
+    },
+
+    '.muted': {
+      'lh.ui.Icon': {
+        opacity: 0.5,
+      },
+
+      'lh.ui.Text': {
+        opacity: 0.5,
+      },
+    },
+
+    'lh.ui.Text': {
+      letterSpacing: 1,
+      paddingVertical: variables.mediumGutter,
+      ...variables.button.text,
+    },
+
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: variables.button.backgroundColor,
+    underlayColor: variables.button.underlayColor,
+  },
+};
