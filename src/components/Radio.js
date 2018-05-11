f6caddb41a4fc2c2c4c7201b41f2b6e82d6192e2
Donald Silveira <donaldsilveira@gmail.com>
2018-05-11 10:26:29 -0300

    Created Radio component

---
+++ src/components/Radio.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { TouchableWithoutFeedback } from 'react-native';
+
+import { View } from './View';
+
+import { connectStyle } from '@shoutem/theme';
+import { connectAnimation, TimingDriver } from '@shoutem/animation';
+
+class Radio extends Component {
+  static propTypes = {
+    value: PropTypes.bool.isRequired,
+    onValueChange: PropTypes.func.isRequired,
+    style: PropTypes.shape({
+      container: PropTypes.object,
+    }),
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.timingDriver = new TimingDriver();
+    this.setValue(props.value);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.value === this.props.value) {
+      return;
+    }
+    this.setValue(nextProps.value);
+  }
+
+  setValue = (value) => {
+    this.timingDriver.toValue(value ? 1 : 0);
+  };
+
+  handlePressed = () => {
+    const { value, onValueChange } = this.props;
+    onValueChange(!value);
+  };
+
+  render() {
+    const { style } = this.props;
+
+    return (
+      <TouchableWithoutFeedback
+        onPress={this.handlePressed}
+        styleName="clear"
+      >
+        <View>
+          <View
+            animationName="thumb"
+            driver={this.timingDriver}
+            style={style.container}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+const AnimatedRadio = connectAnimation(Radio);
+const StyledRadio = connectStyle('lh.ui.Radio')(AnimatedRadio);
+
+export { StyledRadio as Radio };
