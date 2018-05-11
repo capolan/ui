0aa83b3c193d3079a57a4cf5840aec31b84d3257
Donald Silveira <donaldsilveira@gmail.com>
2018-05-11 11:33:28 -0300

    Modulating view styles

---
+++ src/themes/view.js
@@ -0,0 +1,55 @@
+import { INCLUDE } from '@shoutem/theme';
+import { defaultVariables as variables } from './variables';
+
+export default {
+  'lh.ui.View': {
+    [INCLUDE]: ['commonVariants', 'guttersPadding'],
+
+    '.horizontal': {
+      [INCLUDE]: ['horizontalFlexAlignment'],
+      flexDirection: 'row',
+      alignItems: 'flex-end',
+    },
+
+    '.vertical': {
+      [INCLUDE]: ['verticalFlexAlignment'],
+      flexDirection: 'column',
+    },
+
+    // '.fill-parent': {
+    //   [INCLUDE]: ['fillParent'],
+    // },
+
+    // '.overlay': {
+    //   backgroundColor: variables.imageOverlayColor,
+    // },
+
+    // '.overlay-bottom': {
+    //   height: 25,
+    //   bottom: 0,
+    //   left: 0,
+    //   position: 'absolute',
+    //   right: 0,
+    // },
+
+    '.solid': {
+      backgroundColor: variables.paperColor,
+    },
+
+    '.wrap': {
+      flexWrap: 'wrap',
+    },
+
+    '.muted': {
+      opacity: 0.3,
+    },
+
+    '.featured': {
+      [INCLUDE]: ['featuredBackground'],
+    },
+
+    '.center': {
+      alignSelf: 'center',
+    },
+  },
+};
