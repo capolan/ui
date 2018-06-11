24752863ade1ebebe1dd6c9e4b20ad3f82336cc5
Donald Silveira <donaldsilveira@gmail.com>
2018-06-10 21:19:06 -0300

    feat: Created Picker component

---
+++ src/components/Picker.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View } from 'react-native';
+import RMCPicker from 'rmc-picker/lib/Picker';
+import { isChildrenTouchable } from '../utils';
+import { Popup } from '../';
+
+import { connectStyle } from '@shoutem/theme';
+
+class Picker extends Component {
+  static propTypes = {
+    selectedValue: PropTypes.string,
+    onValueChange: PropTypes.func.isRequired,
+    data: PropTypes.arrayOf(PropTypes.shape({
+      value: PropTypes.string,
+      label: PropTypes.string,
+    })).isRequired,
+    children: PropTypes.node,
+  };
+
+  renderChildren = () => {
+    const { children } = this.props;
+    if (isChildrenTouchable(children)) {
+      return React.cloneElement(children, {
+        onPress: () => this.popupRef.open(),
+      });
+    }
+    return children;
+  }
+
+  render() {
+    const { data, selectedValue, onValueChange, style } = this.props;
+    const children = this.renderChildren();
+
+    return (
+      <View>
+        {children}
+        <Popup onRef={ref => { this.popupRef = ref; }}>
+          <RMCPicker
+            itemStyle={style.item}
+            selectedValue={selectedValue}
+            onValueChange={onValueChange}
+          >
+            {data.map(item => (
+              <RMCPicker.Item key={item.value} value={item.value}>
+                {item.label}
+              </RMCPicker.Item>
+            ))}
+          </RMCPicker>
+        </Popup>
+      </View>
+    );
+  }
+}
+
+const StyledPicker = connectStyle('lh.ui.Picker')(Picker);
+
+export { StyledPicker as Picker };
---
+++ src/themes/picker.js
@@ -0,0 +1,11 @@
+import { INCLUDE } from '@shoutem/theme';
+
+export default (variables) => ({
+  'lh.ui.Picker': {
+    item: {
+      fontSize: 22,
+      fontWeight: 'bold',
+      color: variables.darkColor,
+    },
+  },
+});
