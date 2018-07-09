2bc1425a11ae15ac442f35ef9b7f708e8e781363
Donald Silveira <donaldsilveira@gmail.com>
2018-07-09 13:23:17 -0300

    feat: created Collapsible component

---
+++ src/components/Collapsible.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View, TouchableOpacity } from 'react-native';
+
+@observer
+class Collapsible extends Component {
+  static propTypes = {
+    expanded: PropTypes.bool,
+    disabled: PropTypes.bool,
+    children: PropTypes.element.isRequired,
+    headerComponent: PropTypes.func.isRequired,
+    onPress: PropTypes.func,
+  };
+
+  static defaultProps = {
+    expanded: false,
+    disabled: false,
+    onPress: () => null,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isVisible: props.expanded,
+    };
+  }
+
+  toggle = () => {
+    this.setState((prevState) => ({
+      isVisible: !prevState.isVisible
+    }));
+  }
+
+  onPress = () => {
+    const { onPress } = this.props;
+    this.toggle();
+    onPress(this.state.isVisible);
+  }
+
+  render() {
+    const { children, headerComponent, disabled } = this.props;
+    const { isVisible } = this.state;
+
+    return (
+      <View>
+        <TouchableOpacity onPress={() => this.onPress()} disabled={disabled}>
+          {headerComponent(isVisible)}
+        </TouchableOpacity>
+        <View>
+          {isVisible && children}
+        </View>
+      </View>
+    );
+  }
+}
+
+export { Collapsible };
